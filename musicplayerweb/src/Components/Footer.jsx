1241b6dafb6acc78e49788fd5f28e6ba05dacf44
Hoàng Mai <hgmaidg@gmail.com>
2023-04-21 13:24:10 +0700

    Menu Sidebar

--- musicplayerweb/src/Components/Footer.jsx
+++ musicplayerweb/src/Components/Footer.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return <Container>Footer</Container>;
 }
+
+
+const Container = styled.div`
+    background-color: #21212a;
+    height: 100%;
+`;
--- musicplayerweb/src/Components/Sidebar.jsx
+++ musicplayerweb/src/Components/Sidebar.jsx
@@ -1,10 +1,76 @@
 import React from "react";
 import styled from "styled-components";
+import logo from "../Images/musical-note.png";
+import { AiOutlineHome, AiOutlineHeart, AiOutlineCloudUpload } from 'react-icons/ai'
+import { MdOutlineLibraryMusic } from 'react-icons/md'
 
 export default function Sidebar() {
-    return <Container>Sidebar</Container>;
+  return <Container>
+    <div className="top__links">
+      <div className="logo">
+        <img src={logo} alt="LuxMusic" />
+      </div>
+      <ul>
+      <li>
+        <AiOutlineHome fontSize={"1.5rem"}/>
+        <span>Trang Chủ</span>
+      </li>
+        <span>ÂM NHẠC</span>
+        
+      <li>
+        <AiOutlineHeart fontSize={"1.5rem"}/>
+        <span>Đã yêu thích</span>
+      </li>
+      <li>
+        <MdOutlineLibraryMusic fontSize={"1.5rem"}/>
+        <span>Bài hát đã nghe</span>
+      </li>
+      <li>
+        <AiOutlineCloudUpload fontSize={"1.5rem"}/>
+        <span>Đã tải lên</span>
+      </li>
+      <span>DANH SÁCH PLAYLIST</span>
+    </ul>
+    </div>
+    
+  </Container>;
 }
+
 
 const Container = styled.div`
     background-color: #21212a;
+    color: #a5a6c4;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    .top__links {
+      display: flex;
+      flex-direction: column;
+      .logo {
+        text-align: center;
+        margin: 1rem 0;
+        img {
+          max-inline-size: 25%;
+          block-size: auto;
+        }
+      }
+      ul {
+        list-style-type: none;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        padding: 1rem;
+        li {
+          display: flex;
+          gap: 1rem;
+          cursor: pointer;
+          transition: 0.3s ease-in-out;
+          &:hover {
+            color: #ff5f96;
+          }
+        }
+      }
+    }
+
 `;
