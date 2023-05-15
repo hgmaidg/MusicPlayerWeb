ce5c27a2c9556a9fe33c4092ee5c9274db5493c2
Phuong Quyen Le <20520729@gm.uit.edu.vn>
2023-05-15 15:11:06 +0700

    Header

--- musicplayerweb/src/layouts/Components/Header/Headerv2.js
+++ musicplayerweb/src/layouts/Components/Header/Headerv2.js
@@ -1,10 +1,12 @@
 import Media from 'react-media';
-import ProTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-// import classNames from 'classnames/bind';
+import classNames from 'classnames/bind';
 import React, { useRef, useState } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 
+
+import Button from
 function Header() {
     
 }
