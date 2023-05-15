4dec98ce6d81dbd92ffb96805ee83537b60656f8
Hoàng Mai <hgmaidg@gmail.com>
2023-05-15 14:39:12 +0700

    Image

--- musicplayerweb/src/Components/Content/Trending/Trending.js
+++ musicplayerweb/src/Components/Content/Trending/Trending.js
@@ -1,11 +1,54 @@
-import styles from './Trending.css';
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Trending.module.scss';
+
+
+import { ALL_NATIONAL } from '../../../redux/constant';
+import { statusSlice } from '../../../redux/sliceReducer';
+import { combinedStatusSelector } from '../../../redux/selector';
+import { getTrendingDataApi } from '../../../services';
+import Loading from '../../../pages/Loading';
+const cx = classNames.bind(styles);
 
 function Trending() {
+    const dispatch = useDispatch();
+    const { isLoadingPage } = useSelector(combinedStatusSelector);
+    const [dataTrending, setDataTrending] = useState([]);
+    const [paramsFilter, setParamsFilter] = useState(ALL_NATIONAL);
+    const [dataSelect, setDataSelect] = useState([]);
+    const dataSliceRenderRender = dataSelect.slice(0, 12); // slice 12 song to render in content
+
+    const onHandleSelectNational = (item) => {
+        // handle Select National
+        const selectNational = handleSelectButtonNational(item);
+        setParamsFilter(selectNational);
+    };
+
+    useEffect(() => {
+        // paramFilter  change will change dataFilter
+        const dataFilter = handleFilterSongTrending(dataTrending, paramsFilter);
+        setDataSelect(dataFilter);
+    }, [paramsFilter]);
+
+    useEffect(() => {
+        dispatch(statusSlice.actions.isPageLoadingChange(true));
+        const fetch = async () => {
+            const response = await getTrendingDataApi(50);
+            const dataFilter = handleFilterSongTrending(response, paramsFilter);
+            setDataTrending(response);
+            setDataSelect(dataFilter); // default when reload page will use paramFilter recent to render data
+            dispatch(statusSlice.actions.isPageLoadingChange(false));
+        };
+        fetch();
+    }, []);
+
     return (
-        <div className={('wrapper')}>
-            {/* {isLoadingPage && <Loading styles={{ width: '15%', height: '5vh' }} />}
+        <div className={cx('wrapper')}>
+            {isLoadingPage && <Loading styles={{ width: '15%', height: '5vh' }} />}
             {!isLoadingPage && (
                 <div className={cx('title_section')}>
                     <h2>Trending</h2>
@@ -18,28 +61,16 @@ function Trending() {
                         </span>
                     </Link>
                 </div>
-            )} */}
+            )}
 
-            <div className={('title_section')}>
-                <h2>Trending</h2>
-                {/* <Link to="top-trending?_filter=all"> */}
-                <Link style={{color: '#a5a6c4' ,textDecoration: 'none'}}>
-                    <span>
-                        TẤT CẢ
-                        <span className={('chevon_right')}>
-                            {/* <FontAwesomeIcon icon={faChevronRight} /> */}
-                        </span>
-                    </span>
-                </Link>
+            <div className={cx('button_select_national')}>
+                {RenderButtonSelect(paramsFilter, onHandleSelectNational)}
             </div>
-            <div className={('button_select_national')}>
-                {/* {RenderButtonSelect(paramsFilter, onHandleSelectNational)} */}
-            </div>
-            <div className={('container_list_song')}>
-                {/* <RenderFullListSong
+            <div className={cx('container_list_song')}>
+                <RenderFullListSong
                     data={dataSliceRenderRender}
                     HomePageTrending={true}
-                /> */}
+                />
             </div>
         </div>
     );
